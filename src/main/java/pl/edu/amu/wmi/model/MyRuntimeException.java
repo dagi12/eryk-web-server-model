be1dcf5566d134dda7618f5bb5e8d95ff176c298
Eryk Mariankowski <dagi12@o2.pl>
2018-06-22 00:36:29 +0200

    004040 client select

---
+++ src/main/java/pl/edu/amu/wmi/model/MyRuntimeException.java
@@ -0,0 +1,10 @@
+package pl.edu.amu.wmi.model;
+
+/**
+ * Stworzone przez Eryk Mariankowski dnia 22.06.18.
+ */
+public class MyRuntimeException extends RuntimeException {
+    public MyRuntimeException(String message) {
+        super(message);
+    }
+}
