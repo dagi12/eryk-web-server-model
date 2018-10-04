13d645c7ab3289fcef4a00364efb73dfed54a0ef
Eryk Mariankowski <dagi12@o2.pl>
2018-10-04 23:35:50 +0200

    004265 zgromadź projekty

--- src/main/java/pl/edu/amu/wmi/model/MyRuntimeException.java
+++ src/main/java/pl/edu/amu/wmi/model/MyRuntimeException.java
@@ -8,7 +8,7 @@ public class MyRuntimeException extends RuntimeException {
         super(message);
     }
 
-    public MyRuntimeException(NoSuchFieldException e) {
+    public MyRuntimeException(Exception e) {
         super(e);
     }
 }
