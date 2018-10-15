f9e4602261d1a7e49da4fdb4a1e93ab38bae7aec
Eryk Mariankowski <dagi12@o2.pl>
2018-10-15 23:39:59 +0200

    004270 testy Pana Wojtka cz. 2

--- src/main/java/pl/edu/amu/wmi/model/GeneralResponse.java
+++ src/main/java/pl/edu/amu/wmi/model/GeneralResponse.java
@@ -1,12 +1,20 @@
 package pl.edu.amu.wmi.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Stworzone przez Eryk Mariankowski dnia 16.12.17.
  */
+@ApiModel("Response wrapper for target response item")
 public class GeneralResponse<T> {
 
     protected boolean isDone = true;
+
+    @ApiModelProperty("Provided only if done is false")
     private String errorMessage;
+
+    @ApiModelProperty("Provided only if done is true")
     private T item;
 
     public GeneralResponse(String errorMessage) {
