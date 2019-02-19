7ca728478d2e3d0bba99d9138f532f4246ce2b04
Eryk Mariankowski <dagi12@o2.pl>
2019-02-19 13:40:06 +0100

    004582 umożliw eksport z każdej tabeli do pliku Excel, zmianę wykonaj w module flota-web-server-report, tak aby była łatwo dostępna dla innycj aplikacji

---
+++ src/main/java/pl/edu/amu/wmi/model/BaseAttachment.java
@@ -0,0 +1,12 @@
+package pl.edu.amu.wmi.model;
+
+/**
+ * Stworzone przez Eryk Mariankowski dnia 31.08.18.
+ */
+public interface BaseAttachment {
+
+    String getUrl();
+
+    byte[] getPlikBin();
+
+}
