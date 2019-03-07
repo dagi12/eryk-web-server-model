900efc343f12ab4b3808d2946d587e9f225c769e
Eryk Mariankowski <dagi12@o2.pl>
2019-03-07 09:45:38 +0100

    004614 inspekcja kodu, współdzielone stylowanie, skrócenie CompanyUserService

---
+++ src/main/java/pl/edu/amu/wmi/model/FirstLastName.java
@@ -0,0 +1,10 @@
+package pl.edu.amu.wmi.model;
+
+/**
+ * Stworzone przez Eryk Mariankowski dnia 07.03.19.
+ */
+public interface FirstLastName {
+    String getImie();
+
+    String getNazwisko();
+}
